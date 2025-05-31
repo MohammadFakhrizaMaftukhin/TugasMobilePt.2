fc80719fda4ef4400cae065dfae864104467558e
MohammadFakhrizaMaftukhin <fakhriza18b@gmail.com>
2025-05-31 12:43:43 +0700

    penambahan daftar produk dengan berbagai item jus

--- app/src/main/java/com/example/multiplerv/MainActivity.kt
+++ app/src/main/java/com/example/multiplerv/MainActivity.kt
@@ -22,13 +22,14 @@ class MainActivity : AppCompatActivity(), ProductAdapter.OnProductChangeListener
         }
 
         val productList = arrayListOf(
-            Product("Jus", 0, 25000),
-            Product("Jus", 0, 30000),
-            Product("Jus", 0, 31000),
-            Product("Jus", 0, 32000),
-            Product("Jus", 0, 33000),
-            Product("Jus", 0, 34000),
-            Product("Jus", 0, 35000),
+            Product("Jus Jeruk", 0, 25000, false, R.drawable.orange),
+            Product("Jus Mangga", 0, 30000, false, R.drawable.mango),
+            Product("Jus Alpukat", 0, 31000, false, R.drawable.avocado),
+            Product("Jus Strawberry", 0, 32000, false, R.drawable.strawberry),
+            Product("Jus Anggur", 0, 33000, false, R.drawable.grape),
+            Product("Jus Semangka", 0, 34000, false, R.drawable.watermelon),
+            Product("Jus Melon", 0, 35000, false, R.drawable.melon),
+            Product("Jus Jambu", 0, 36000, false, R.drawable.guava)
         )
 
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
