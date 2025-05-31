53f2cf4abf52642d8e02cf0c99a4d6636d36a8d4
MohammadFakhrizaMaftukhin <fakhriza18b@gmail.com>
2025-05-31 12:21:50 +0700

    penambahan imageResId untuk penyimpanan referensi gambar

--- app/src/main/java/com/example/multiplerv/Product.kt
+++ app/src/main/java/com/example/multiplerv/Product.kt
@@ -1,3 +1,9 @@
 package com.example.multiplerv
 
-data class Product(val name: String, var count : Int, val price : Int, var isFavorite: Boolean = false)
+data class Product(
+    val name: String,
+    var count : Int,
+    val price : Int,
+    var isFavorite: Boolean = false,
+    val imageResId: Int
+)
